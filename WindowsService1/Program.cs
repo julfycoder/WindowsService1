d2b623cdab1ddd2687303afeba9d6249754dc389
Bogdan Besedin <Bogdan Besedin>
2017-02-16 10:22:25 +0200

    First commit

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            Service1 s = new Service1();
+            s.OnDebug();
+            Thread.Sleep(Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            FileStream fs = File.Create(AppDomain.CurrentDomain.BaseDirectory + "Hello_World_File.txt");
+            StreamWriter writer = new StreamWriter(fs);
+            writer.WriteLine("Hi, you've wrote this message from windows service!!");
+            writer.Close();
+            fs.Close();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
